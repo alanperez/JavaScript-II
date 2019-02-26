569033d178e70cfa3037e916f377f8dd164f1393
Alan Perez <alanp9six@gmail.com>
2019-02-26 15:58:37 -0600

    Closure challenges 1-2 are completed

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,52 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function makeCake() 
+    {
+       var flavor = "Red Velvet";
+        function displayFlav()
+        {
+          console.log(flavor);
+        }
+        return displayFlav;
+    }
+      var myFun = makeCake();
+      myFun()
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
+let count = 0;
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+return ++count;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
+console.log(counter());
+console.log(counter());
+console.log(counter());
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+    
+
+
+let count = 0;
+
+  function increment(){
+    ++count;
+    return count;
+  }
+  increment();
+  // function decrement(){
+  //   --count;
+  //   return count;
+  // }
+  console.log(increment(count));
+console.log(increment(count));
+  };
+
