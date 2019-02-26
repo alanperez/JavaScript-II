6784e5ac3cd75743a215b5edd3cccbb620094462
Alan Perez <alanp9six@gmail.com>
2019-02-26 14:08:29 -0600

    array-method completed using ES5 Way

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,65 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
-console.log(fullName);
+
+//ES5
+
+    for(let i = 0; i < runners.length; i++) {
+        function addName()
+        {
+            console.log(`${runners[i].first_name} ${runners[i].last_name}`);
+        }
+        addName();
+    }
+
+//ES6
+// runners.forEach( (run) => {
+// fullName.push(`${run.first_name} ${run.last_name}`);
+// })
+// console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+//ES5 WAY
+for(let i = 0; i < runners.length; i++) {
+  function runnerName() {
+    allCaps.push(runners[i].first_name.toUpperCase());
+  }
+  runnerName();
+}
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+//ES5
+for(let i = 0; i < runners.length; i++)
+{
+    function runnerListFilter() 
+    {
+        if(runners[i].shirt_size === 'L') 
+        {
+            largeShirts.push(runners[i].first_name +" "+ runners[i].last_name);
+        }
+    }
+    runnerListFilter();
+}
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+// let ticketPriceTotal = [];
+    //ES5 WAY
+let ticketPriceTotal = 0;
+    for(let i = 0; i < runners.length; i++)
+    {
+        function addTickets()
+        {
+            ticketPriceTotal += runners[i].donation;
+        }
+        addTickets();
+    }
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
